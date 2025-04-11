2ece3e04fe41b56006213c0b26c6fd882f582a35
Chidimma Ijoma <nevusijoma@gmail.com>
2025-04-11 16:12:54 +0100

    Improve the weather client to show the 3 hourly forecast for 5 days

--- weather_client.py
+++ weather_client.py
@@ -1,16 +1,51 @@
 import requests
+from datetime import datetime
 
 api_key =open('api.txt','r').read()
 
-user_input = input ("Enter city: ")
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&appid={api_key}"
-    )
+user_input = input('Name of the place: ')
 
-if weather_data.json() ['cod'] == '404':
-    print("No City Found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
-    print (f"The weather in {user_input} is: {weather}")
-    print (f"The temperature in {user_input} is: {temp}°C")
+base_url = "http://api.openweathermap.org/data/2.5/forecast"
+complete_url = base_url + "?q=" + user_input + "&appid=" + api_key 
+#print(complete_url)
+
+
+json_data = requests.get(complete_url).json()
+#print(json_data)
+
+
+name_of_the_city = json_data['city']['name']
+print(name_of_the_city)
+city_lat = json_data['city']['coord']['lat']
+city_lon = json_data['city']['coord']['lon']
+print("latitude : " + str(city_lat) + ", Longitude : " + str(city_lon))
+
+#Let's now format the output to make it readable
+string = f'[ {name_of_the_city} - 5 days forecast]\n'
+
+
+for item in json_data['list']:
+    time_forecasted = item['dt_txt']
+    time_forecasted = datetime.strptime(time_forecasted, '%Y-%m-%d %H:%M:%S')
+    day_forcasted = datetime.strftime(time_forecasted,'%A')
+    print("\n" + str(day_forcasted)+ " " + str(time_forecasted))
+    temp = str(int(item['main']['temp']-273.15))
+    print("Temperature in Celsius: " + temp)
+    feels_like = str(int(item['main']['feels_like'] - 273.15))
+    print("Temperature in Celsius feels like: " + feels_like)
+    pressure = str(item['main']['pressure'])
+    print("Atmospheric pressure on sea level in hPa: " + pressure)
+    humidity = str(item['main']['humidity'])
+    print("Humidity: "+ humidity + "%")
+    weather_des = item['weather'][0]['description']
+    print("Weather description: "+ weather_des.title())
+    cloudiness = str(item['clouds']['all'])
+    print("Cloudiness: " + cloudiness + "%")
+    windSpd = str(item['wind']['speed'])
+    print("Wind speed: \n\t in meter per second is " + windSpd)
+    windSpd = str(float(item['wind']['speed']*3600/1000))
+    print("\t in kilometer per second is " + windSpd)
+    wind_degree = str(item['wind']['deg'])
+    print("Wind degree: " + wind_degree + "°")
+    pop = str(int(item['pop']*100))
+    print("Probability of Precipitation (Rain): " + pop + "%") 
